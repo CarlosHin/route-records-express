d5a0a2b408f51f90063e182ba77f50cd2cc6f072
Carlos <carloshin98@gmail.com>
2023-03-23 20:47:41 +0100

    allow all requests

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -5,12 +5,16 @@ import { userRouter } from "../api/public/users/userRoutes";
 import { loginRouter } from "../api/public/login/loginRoutes";
 import { isLogged } from "../middlewares/isLogged";
 import { routesRouter } from "../api/private/routes/routesRoutes";
+import { addHeaders } from "../middlewares/addHeaders";
 
 export default async ({ app }: { app: express.Application }) => {
   app.use(json());
+  app.use(addHeaders);
   app.use("/api/public/users", userRouter);
   app.use("/api/public/login", loginRouter);
   app.use("/api/private/routes", isLogged, routesRouter);
-  app.use(cors());
+  app.use(cors({
+    origin: '*'
+  }));
   return app;
 };
---
+++ src/middlewares/addHeaders.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction} from "express";
+import { getTokenFrom } from "../utils/tokenUtilities";
+import  jwt from "jsonwebtoken";
+
+
+export const addHeaders  = (req: Request, res:Response, next:NextFunction) => {
+    try{
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+        );
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        next();
+    }catch(error){
+        if (error instanceof Error) return res.status(500).send(error.message);
+        return res.status(500).send("Unknown error Auth");
+    }
+};
