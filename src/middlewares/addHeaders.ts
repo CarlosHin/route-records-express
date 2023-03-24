63c0d75ff4fa3d9852746dd953034fbc074e802f
Carlos <carloshin98@gmail.com>
2023-03-24 22:40:53 +0100

    change for cors

--- src/middlewares/addHeaders.ts
+++ src/middlewares/addHeaders.ts
@@ -10,7 +10,10 @@ export const addHeaders  = (req: Request, res:Response, next:NextFunction) => {
           "Access-Control-Allow-Methods",
           "OPTIONS, GET, POST, PUT, PATCH, DELETE"
         );
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+        if (req.method === "OPTIONS") {
+            return res.status(200).end();
+        }
         next();
     }catch(error){
         if (error instanceof Error) return res.status(500).send(error.message);
